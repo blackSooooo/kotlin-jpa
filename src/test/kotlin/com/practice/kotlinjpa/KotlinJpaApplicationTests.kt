3af4095de71ae511d914aeb5998a47c6276d42d5
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2022-11-12 18:41:54 +0900

    chore: 초기 프로젝트 세팅

---
+++ src/test/kotlin/com/practice/kotlinjpa/KotlinJpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.practice.kotlinjpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinJpaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
