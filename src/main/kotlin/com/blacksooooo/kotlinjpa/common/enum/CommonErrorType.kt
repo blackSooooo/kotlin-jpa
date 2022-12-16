4455ee5d46f4327e0596f9667b1bb179b808c4a6
blackSooooo <blacksooooo@naver.com>
2022-12-16 23:48:47 +0900

    feat: NotDeliveryCanceledException, 에러 타입 추가

--- src/main/kotlin/com/blacksooooo/kotlinjpa/common/enum/CommonErrorType.kt
+++ src/main/kotlin/com/blacksooooo/kotlinjpa/common/enum/CommonErrorType.kt
@@ -3,5 +3,6 @@ package com.blacksooooo.kotlinjpa.common.enum
 enum class CommonErrorType(
     val message: String,
 ) {
-    LACK_OF_STOCK_QUANTITY("재고가 충분하지 않습니다.")
+    LACK_OF_STOCK_QUANTITY("재고가 충분하지 않습니다."),
+    ALREADY_DELIVERY_COMPLETED("이미 배송이 완료되었습니다.")
 }
