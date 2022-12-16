51fd19fcef98e24134afe07f40386f813f9e041e
blackSooooo <blacksooooo@naver.com>
2022-12-16 23:49:15 +0900

    fix: 트랜잭션 어노테이션 추가

--- src/main/kotlin/com/blacksooooo/kotlinjpa/domain/delivery/DeliveryWriter.kt
+++ src/main/kotlin/com/blacksooooo/kotlinjpa/domain/delivery/DeliveryWriter.kt
@@ -3,11 +3,13 @@ package com.blacksooooo.kotlinjpa.domain.delivery
 import com.blacksooooo.kotlinjpa.storage.delivery.DeliveryEntity
 import com.blacksooooo.kotlinjpa.storage.delivery.DeliveryRepository
 import org.springframework.stereotype.Component
+import javax.transaction.Transactional
 
 @Component
 class DeliveryWriter(
     private val deliveryRepository: DeliveryRepository
 ) {
+    @Transactional
     fun save(city: String, street: String, zipCode: String): Long {
         val delivery = deliveryRepository.save(
             DeliveryEntity(city, street, zipCode)
